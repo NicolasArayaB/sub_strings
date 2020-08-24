53e605de5e8a0cc2a5c646ea9c48b3f233b5af42
NicolasArayaB <nicolasaraya@gmail.com>
2020-08-24 11:58:56 -0400

    this just count if theres a match

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,1 +1,14 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(str, substr_array)
+    hash = Hash.new(0)
+    string = str.downcase
+    substr_array.each do |substr|
+        if string.count {|substr| substr == string }
+            hash[substr] += 1 
+        end
+    end
+    puts hash
+end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
